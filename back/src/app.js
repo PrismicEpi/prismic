dd590adfa1f28403129e8dae5358f04688a05e9e
Platchoon <yplatoonmail2@gmail.com>
2025-05-08 17:34:40 +0200

    swagger added

--- back/src/app.js
+++ back/src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cors from "cors";
+import expressJSDocSwagger from "express-jsdoc-swagger";
+import { swaggerOptions } from "./config/swagger.js";
+
+// import authMiddleware from "./middlewares/authMiddleware.js";
+// import keystroke from "./routes/keystrokeRoute.js";
 import authRoute from "./routes/authRoute.js";
 import experimentRoute from "./routes/experimentRoute.js";
 const app = express();
@@ -18,6 +23,10 @@ app.use(cors(corsOptions));
 // Body parser
 app.use(express.json());
 // TODO : Add a Logger, voir pour un service tiers qui fait ça bien avec de l'alerting pourquoi pas, ou posthog
+
+
+// Initialize Swagger
+expressJSDocSwagger(app)(swaggerOptions);
 
 // Routes
 app.use("/api", authRoute);
